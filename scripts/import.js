1c2af9cd882d91593ecb038e6bd48f69273006f6
Frane Cagalj <fcagalj@gmail.com>
2018-12-06 13:09:48 +0100

    importer script

--- scripts/import.js
+++ scripts/import.js
@@ -1,5 +1,7 @@
-const execa = require('execa');
-const async = require('async');
+const execa      = require('execa');
+const async      = require('async');
+const options    = require('./../default-options.json');
+
 
 function _getCommand() {
   return 'mongodump';
@@ -10,7 +12,9 @@ function _getArguments(opt) {
   for (var key in opt) {
     if (opt.hasOwnProperty(key)) {
       _a.push('--' + key);
-      _a.push(opt[key]);
+      if(opt[key]){
+        _a.push(opt[key]);
+      }
     }
   }
   return _a;
@@ -22,25 +26,15 @@ function mongoexport(opt) {
 
 
       return result;
-      // return result.stdout || 'Success!';
     }).catch(err => {
       console.log('Error mongoexport: ', err);
     });
 }
 
-var opt = {
-  host: '127.0.0.1:27017', //<hostname><:port>  Default: localhost:27017
-  db: 'bb_editing',
-  // out: '-'
-  // collection: 'user',
-  // fields: 'user,email,contact',
-  // out: 'users.csv',
-  // type: 'csv'
-};
 //mongoexport command should be in path variable
 //all options for mongoexport command can be used
 
-mongoexport(opt)
+mongoexport(options.import)
   .then(result => {
     console.log('Export result: ', result);
   })
